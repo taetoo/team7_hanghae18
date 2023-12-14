1a7e095bdedea91b504810f40d77c6f273d23c6c
taetoo <rlaougs5@naver.com>
2023-12-14 19:15:48 +0900

    first-commit

---
+++ git_prac/src/test/java/com/example/git_prac/GitPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
